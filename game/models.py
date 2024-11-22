fd38d8c402402807f3ce8e093dda3cca9bcf660b
Herbie James <herbiejames.dm@gmail.com>
2024-11-22 11:33:17 +0000

    add description attribute to Game model

--- game/models.py
+++ game/models.py
@@ -6,6 +6,7 @@ STATUS = ((0, "In Progress"), (1, "Installed"))
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
     slug = models.SlugField(max_length=200, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
