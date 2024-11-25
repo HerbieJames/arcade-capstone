a907ac6612b6945af5bc418c5cba5b1e8e04f3dd
Herbie James <herbiejames.dm@gmail.com>
2024-11-25 10:18:37 +0000

    init Score model to game app

--- game/models.py
+++ game/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+GAME = ((0, "Frogger"), (1, "Snake"))
+
 # Create your models here.
+class Score(models.Model):
+    game        = models.IntegerField(choices=GAME, default=0)
+    player      = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="personal_scores", null=True, blank=True
+    )
+    alias       = models.CharField(max_length=6)
+    created_on  = models.DateTimeField(auto_now_add=True)
+    value       = models.CharField(max_length=6)
