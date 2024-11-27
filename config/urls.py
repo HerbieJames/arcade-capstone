3ef74b2013654e0f8e5902ff2d6caf6c77614948
Herbie James <herbiejames.dm@gmail.com>
2024-11-27 10:15:30 +0000

    refactor project. moved index view to game app. depreciated home app.

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home import views as index_views
-from game import views as game_views
+from game import views as index_views
 
 urlpatterns = [
     path('', index_views.index, name="home"),
-    path('game/', game_views.machine, name="games"),
+    path('game/', index_views.machine, name="game"),
     path('admin/', admin.site.urls),
 ]
--- game/views.py
+++ game/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def machine(request):
     return HttpResponse("<h1>G A M E - M A C H I N E</h1>")
+
+def index(request):
+    return HttpResponse("<p style='color: blue; text-align: center;'>home</p>")
