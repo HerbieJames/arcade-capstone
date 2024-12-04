293348c91f56d025f9e2b417effcb38e9ed2516f
Herbie James <herbiejames.dm@gmail.com>
2024-12-04 21:47:28 +0000

    add authentication

--- game/urls.py
+++ game/urls.py
@@ -1,9 +1,10 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('', views.GameList.as_view(), name="home"),
+    path("accounts/", include("allauth.urls")),
     path('snake/', views.SnakeMachine.as_view(), name='snake'),
     path('frogger/', views.FroggerMachine.as_view(), name='frogger'),
 ]
