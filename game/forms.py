e262b3ddad578417964c85a897081ff34ada8537
Herbie James <herbiejames.dm@gmail.com>
2024-12-11 20:03:48 +0000

    make score field readonly in forms.py

--- game/forms.py
+++ game/forms.py
@@ -3,6 +3,12 @@ from django import forms
 
 
 class ScoreForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):  # Prohibts manual change of "value" field
+        super(ScoreForm, self).__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)
+        if instance and instance.pk:
+            self.fields['value'].widget.attrs['readonly'] = True
+
     class Meta:
         model = Score
         fields = ('value', 'alias',)
